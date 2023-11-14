f399f58488473a0b6475d1270cbc35bf929ddf8a
Olexander <dneprovsky1997@icloud.com>
2023-11-14 13:01:48 +0100

    hw 1

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// console.log(galleryItems);
+
+
+const galery = document.querySelector('.gallery')
+
+
+function createMurkup(arr) {
+    return arr.map(({ preview, original, description }) => 
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    ).join("")
+}
+
+galery.insertAdjacentHTML('beforeend', createMurkup(galleryItems))
+galery.addEventListener('click', hendleClick)
+
+function hendleClick(event) {
+    event.preventDefault()
+    if (event.target === event.currentTarget) {
+        return
+    }
+
+    const modalOn = basicLightbox.create(
+    `<img src="${event.target.dataset.sourse}" />`
+    )
+    modalOn.show()
+    
+}
+
+
+
+
+
